f5019e609baa3dd25173a4ea173a324ba3539169
Michael Minella <mminella@pivotal.io>
2019-08-30 11:09:19 -0500

    Initial commit

---
+++ src/main/java/io/spring/versionedbatchjob/VersionedBatchJobApplication.java
@@ -0,0 +1,13 @@
+package io.spring.versionedbatchjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionedBatchJobApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(VersionedBatchJobApplication.class, args);
+	}
+}
---
+++ src/main/java/io/spring/versionedbatchjob/configuration/BatchConfiguration.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.spring.versionedbatchjob.configuration;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.PropertySource;
+
+/**
+ * @author Michael Minella
+ */
+@EnableBatchProcessing
+@EnableTask
+@Configuration
+public class BatchConfiguration {
+
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+	@Autowired
+	private Environment environment;
+
+	@Autowired
+	private VersionedBatchJobProperties versionedBatchJobProperties;
+
+	@Bean
+	public Job job() {
+		return this.jobBuilderFactory.get("job")
+				.start(step1())
+				.build();
+	}
+
+	@Bean
+	public Step step1() {
+		return this.stepBuilderFactory.get("step1")
+				.tasklet(new EnvironmentalTasklet(this.environment, this.versionedBatchJobProperties.getWait()))
+				.build();
+	}
+
+	public static class EnvironmentalTasklet implements Tasklet {
+
+		final private Environment environment;
+
+		final private long wait;
+
+		public EnvironmentalTasklet(Environment environment, long wait) {
+			this.environment = environment;
+			this.wait = wait;
+		}
+
+		@Override
+		public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+			String name = new File(BatchConfiguration.class.getProtectionDomain()
+					.getCodeSource()
+					.getLocation().getPath())
+					.getParentFile()
+					.getParentFile()
+					.getName();
+
+			Map<String, Object> properties = this.getProperties();
+
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			String jsonProperties = gson.toJson(properties);
+
+			System.out.println(">> The jar file being executed is = " + name.substring(0, name.length() - 1));
+
+			System.out.println("********** PROPERTIES *************");
+			System.out.println(jsonProperties);
+
+			Thread.sleep(this.wait);
+
+			return RepeatStatus.FINISHED;
+		}
+
+		private Map<String, Object> getProperties() {
+			Map<String, Object> map = new HashMap<>();
+
+			for(Iterator it = ((AbstractEnvironment) this.environment).getPropertySources().iterator(); it.hasNext(); ) {
+				PropertySource propertySource = (PropertySource) it.next();
+				if (propertySource instanceof MapPropertySource) {
+					map.putAll(((MapPropertySource) propertySource).getSource());
+				}
+			}
+
+			return map;
+		}
+	}
+}
